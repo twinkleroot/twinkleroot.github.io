56b555dffc17a7a76225f2ee895c4868c1b157c1
justin <jmahn@lunasoft.co.kr>
2020-11-25 16:06:57 +0900

    calendar 적용

--- lib.js
+++ lib.js
@@ -8,17 +8,6 @@ function get_json_path() {
     return year + month + '.json';
 }
 
-/**
- *  yyyy-MM-dd 포맷으로 반환
- */
-function get_format_date(yymmdd) {
-    const year = yymmdd.substring(0,2);
-    const month = yymmdd.substring(2,4);
-    const day = yymmdd.substring(4,6);
-
-    return '20' + year + '-' + month + '-' + day;
-}
-
 function get_day_diff(_date1, _date2) {
     var diff_date_1 = _date1 instanceof Date ? _date1 :new Date(_date1);
     var diff_date_2 = _date2 instanceof Date ? _date2 :new Date(_date2);
--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 (function() {
-    const path = get_json_path();
-    fill_html_data(path);
+    // 그 달의 달력 추가
+    show_calendar(new Date());
 
-    // 저번 달 기록을 선택해서 다시 그리게 하는 것도 필요. (페이징)
+    fill_html_data(get_json_path());
+    // 이전, 이후 달력 버튼 만들어서 저번달 데이터 로드
 }) ();
+
+function show_calendar(date) {
+    const section = document.querySelector('section');
+    const dom_firstday = (new Date(date.getFullYear(), date.getMonth(), 1)).getDay();
+    const last_date = (new Date(date.getFullYear(), date.getMonth()+1, 0)).getDate();
+    const title_div = document.querySelector('.calendar-title>strong');
+    title_div.textContent = (date.getMonth() + 1) + '월';
+    
+    const base_day = last_date-(7-dom_firstday);    // 이 달의 일수 - (일주일 - 첫번째날의 요일값)이 기준일.
+    const last_index = get_calendar_row(base_day) * 7;
+
+    for(let index=0; index<last_index; index++) {
+        const div = document.createElement('div');
+        const real_date = index - dom_firstday + 1;
+        current_row = parseInt(index / 7);
+        current_column = index % 7;
+        if(index < dom_firstday || index >= last_date+dom_firstday) {
+            div.textContent = '-';
+        } else {
+            div.setAttribute('data-row', current_row);
+            div.setAttribute('data-column', current_column);
+            div.classList.add('day-'+real_date);
+            div.textContent = real_date;
+        }
+        div.classList.add('calendar-day');
+        section.appendChild(div);
+    }
+}
 
 function fill_html_data(path) {
     let request = new XMLHttpRequest();
@@ -15,30 +44,39 @@ function fill_html_data(path) {
     }
 }
 
+function get_calendar_row(base_day) {
+    let row = 4;
+    if(base_day - 28 > 0) {
+        row = 6;
+    } else if(base_day - 28 <= 0) {
+        row = 5;
+    }
+
+    return row;
+}
+
 function show_daily_log(response) {
-    let section = document.querySelector('section');
-    let header_count_area = document.querySelector('header>h1');
-    let daily_count = document.createElement('span');
-    daily_count.textContent = '식단변경 ' + get_day_diff('2020-11-09', new Date()) + '일차';
-    header_count_area.appendChild(daily_count);
+    const header_count_area = document.querySelector('header>h1');
+    const start_date = '2020-11-09';
+    header_count_area.textContent = '식단변경 ' + get_day_diff(start_date, new Date()) + '일차';
 
-    response.forEach(element => {
-        let article = document.createElement('article');
-        let date = document.createElement('h3');
-        let title = document.createElement('p');
-        let weight = document.createElement('p');
-        let memo = document.createElement('p');
+    for (const [key, value] of Object.entries(response[0])) {
+        const article_el = document.createElement('article');
+        const title_el = document.createElement('h3');
+        const weight_el = document.createElement('p');
+        const memo_el = document.createElement('p');
+        const day_el = document.querySelector('.day-'+key);
 
-        title.textContent = element.title;
-        date.textContent = get_format_date(element.date);
-        weight.textContent = element.weight + 'kg';
-        memo.textContent = element.memo;
+        title_el.textContent = value.title;
+        weight_el.textContent = value.weight + 'kg';
+        memo_el.textContent = value.memo;
+        memo_el.classList.add('no-display');
 
-        article.appendChild(date);
-        article.appendChild(title);
-        article.appendChild(weight);
-        article.appendChild(memo);
+        article_el.appendChild(title_el);
+        article_el.appendChild(weight_el);
+        article_el.appendChild(memo_el);
 
-        section.appendChild(article);
-    });
+        day_el.classList.add('look-content');
+        day_el.appendChild(article_el);
+    };
 }
